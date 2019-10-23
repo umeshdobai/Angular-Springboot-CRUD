2dbeba82f63ea9adcefb2c23d0753184495616df
umeshdobai <umeshdobai@gmail.com>
2019-10-23 16:33:00 +0530

    Update student.component.ts

--- Angular Code/src/Student/student.component.ts
+++ Angular Code/src/Student/student.component.ts
@@ -3,8 +3,7 @@ import { Student } from "src/Student/student";
 import { StudentService } from "src/Student/student.service";
 @Component({
 	selector:'student',
-	templateUrl:'./student.component.html',
-	styleUrls:['./student.component.css']
+	templateUrl:'./student.component.html'
 })
 export class StudentComponent implements OnInit{
 	stud = new Student();
